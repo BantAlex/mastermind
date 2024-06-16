cbd1ecce4763a73ec265798dc43157f427fed501
BantAlex <bantalex3@gmail.com>
2024-06-16 22:14:17 +0300

    Make choice checks for length and inclusion

--- lib/secret_code.rb
+++ lib/secret_code.rb
@@ -1,10 +1,11 @@
 require "./lib/play_area"
 class SecretCode
-  attr_accessor :colors, :secret_code, :match
+  attr_accessor :colors, :secret_code, :match, :choice
 
   def initialize
     self.colors = %w[R G B M C Y]
     @match = PlayArea.new
+    make_choice
   end
 
   def generate_code
@@ -12,7 +13,25 @@ class SecretCode
   end
 
   def make_choice
-    choice = gets.chomp
+    print "Please select your colors(A1-D1): "
+    self.choice = gets.chomp.upcase.split
+
+    # check for length
+    if choice.length > 4
+      puts "That's too many colors!"
+      make_choice
+    elsif choice.length < 4
+      puts "You need #{4 - choice.length} more color(s)!"
+      make_choice
+    end
+
+    # check for inclusion
+    choice.each do |chosen_color|
+      next if colors.include?(chosen_color)
+
+      puts "#{chosen_color} is not a valid option"
+      make_choice
+    end
   end
 end
 

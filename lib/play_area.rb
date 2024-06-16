819eea13e79336ba928d4eaaacf3c63193032e8f
BantAlex <bantalex3@gmail.com>
2024-06-16 15:41:26 +0300

    Remove redundant method

--- lib/play_area.rb
+++ lib/play_area.rb
@@ -1,53 +1,48 @@
 require "colorize"
 class PlayArea
-  attr_accessor :rows, :pos_name
+  attr_accessor :position
 
   def initialize
     clear_board
   end
 
   def clear_board
-    self.rows = {}
+    self.position = {}
     ("A".."D").each do |c|
       (1..12).each do |r|
         pos_name = c.to_s + r.to_s
-        rows[pos_name] = " "
+        position[pos_name] = " "
       end
     end
     board
   end
 
-  def pos_gen(row, column)
-    @pos_name = column.to_s + row.to_s
-    rows[pos_name]
-  end
-
   def board
     puts "     A   B   C   D |    F    |"
     puts "   |⎺⎺⎺|⎺⎺⎺|⎺⎺⎺|⎺⎺⎺|⎺⎺⎺⎺⎺⎺⎺⎺⎺|"
-    puts "1  | #{pos_gen(1, 'A')} | #{pos_gen(1, 'B')} | #{pos_gen(1, 'C')} | #{pos_gen(1, 'D')} | Feedback"
+    puts "1  | #{position['A1']} | #{position['B1']} | #{position['C1']} | #{position['D1']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "2  | #{pos_gen(2, 'A')} | #{pos_gen(2, 'B')} | #{pos_gen(2, 'C')} | #{pos_gen(2, 'D')} | Feedback"
+    puts "2  | #{position['A2']} | #{position['B2']} | #{position['C2']} | #{position['D2']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "3  | #{pos_gen(3, 'A')} | #{pos_gen(3, 'B')} | #{pos_gen(3, 'C')} | #{pos_gen(3, 'D')} | Feedback"
+    puts "3  | #{position['A3']} | #{position['B3']} | #{position['C3']} | #{position['D3']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "4  | #{pos_gen(4, 'A')} | #{pos_gen(4, 'B')} | #{pos_gen(4, 'C')} | #{pos_gen(4, 'D')} | Feedback"
+    puts "4  | #{position['A4']} | #{position['B4']} | #{position['C4']} | #{position['D4']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "5  | #{pos_gen(5, 'A')} | #{pos_gen(5, 'B')} | #{pos_gen(5, 'C')} | #{pos_gen(5, 'D')} | Feedback"
+    puts "5  | #{position['A5']} | #{position['B5']} | #{position['C5']} | #{position['D5']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "6  | #{pos_gen(6, 'A')} | #{pos_gen(6, 'B')} | #{pos_gen(6, 'C')} | #{pos_gen(6, 'D')} | Feedback"
+    puts "6  | #{position['A6']} | #{position['B6']} | #{position['C6']} | #{position['D6']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "7  | #{pos_gen(7, 'A')} | #{pos_gen(7, 'B')} | #{pos_gen(7, 'C')} | #{pos_gen(7, 'D')} | Feedback"
+    puts "7  | #{position['A7']} | #{position['B7']} | #{position['C7']} | #{position['D7']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "8  | #{pos_gen(8, 'A')} | #{pos_gen(8, 'B')} | #{pos_gen(8, 'C')} | #{pos_gen(8, 'D')} | Feedback"
+    puts "8  | #{position['A8']} | #{position['B8']} | #{position['C8']} | #{position['D8']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "9  | #{pos_gen(9, 'A')} | #{pos_gen(9, 'B')} | #{pos_gen(9, 'C')} | #{pos_gen(9, 'D')} | Feedback"
+    puts "9  | #{position['A9']} | #{position['B9']} | #{position['C9']} | #{position['D9']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "10 | #{pos_gen(10, 'A')} | #{pos_gen(10, 'B')} | #{pos_gen(10, 'C')} | #{pos_gen(10, 'D')} | Feedback"
+    puts "10 | #{position['A10']} | #{position['B10']} | #{position['C10']} | #{position['D10']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "11 | #{pos_gen(11, 'A')} | #{pos_gen(11, 'B')} | #{pos_gen(11, 'C')} | #{pos_gen(11, 'D')} | Feedback"
+    puts "11 | #{position['A11']} | #{position['B11']} | #{position['C11']} | #{position['D11']} | Feedback"
     puts "   |⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|⎻⎻⎻|"
-    puts "12 | #{pos_gen(12, 'A')} | #{pos_gen(12, 'B')} | #{pos_gen(12, 'C')} | #{pos_gen(12, 'D')} | Feedback"
+    puts "12 | #{position['A12']} | #{position['B12']} | #{position['C12']} | #{position['D12']} | Feedback"
     puts "   |⎽⎽⎽|⎽⎽⎽|⎽⎽⎽|⎽⎽⎽|"
     puts ""
   end
